962b408d489571077912e2bac7b6bf45d8601cdb
Yashpal Singh <yashpalsingh9rx@outlook.com>
2025-03-04 20:51:45 +0530

    checking cors

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -8,7 +8,7 @@ export default {
   AT_SECRET: process.env.AT_SECRET || '',
   DEV_WEB_URL: 'http://localhost:3000',
   PROD_WEB_URL: 'https://task.yshplsngh.in',
-  PROD_WEB_URL_2: 'https://task-manager-liard-nine.vercel.app/',
-  ATTL: '10s',
+  PROD_WEB_URL_2: 'https://task-manager-liard-nine.vercel.app',
+  ATTL: '10m',
   RTTL: '1y',
 };
